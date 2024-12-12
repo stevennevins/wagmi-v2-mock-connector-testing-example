02f044b93530ee3fe3dbb8656ca79b18fc09fe9f
steven <12021290+stevennevins@users.noreply.github.com>
2024-12-11 20:19:58 -0500

    feat: anvil.js instead (#1)
    * feat: anvil.js instead
    
    * ci: add ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vitest/config'
 
- export default defineConfig({
-     test: {
-         environment: 'jsdom',
-         exclude: ['**/node_modules/**', 'public/**'],
-         setupFiles: ['./vitest.setup.ts'],
-         globals: true,
-     },
- })
+export default defineConfig({
+    test: {
+        environment: 'jsdom',
+        exclude: ['**/node_modules/**', 'public/**'],
+        setupFiles: ['./vitest.setup.ts'],
+        globals: true,
+    },
+})
