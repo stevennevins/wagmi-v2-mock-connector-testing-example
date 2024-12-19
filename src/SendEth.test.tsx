e46be45586937de3eabe029af5f5559a0be0a03b
steven nevins <steven.nevins.js@gmail.com>
2024-12-19 07:51:44 -0500

    test: assert unique anvil instances

--- src/SendEth.test.tsx
+++ src/SendEth.test.tsx
@@ -3,6 +3,10 @@ import { act, screen, fireEvent, waitFor } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import Connect from "./Connect";
 import SendEth from "./SendEth";
+import { parseEther } from "viem";
+import { createPublicClient, http } from "viem";
+import { anvil } from "../test/utils";
+import { mockAccount } from "../test";
 
 describe("SendEth", () => {
   it("should render the SendEth component", async () => {
@@ -20,6 +24,23 @@ describe("SendEth", () => {
   });
 
   it("should handle sending ETH", async () => {
+    const publicClient = createPublicClient({
+      chain: anvil,
+      transport: http(),
+    });
+
+    const recipient = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
+    const amount = parseEther("0.01"); // Default amount in SendEth.tsx
+
+    const senderBalanceBefore = await publicClient.getBalance({
+      address: mockAccount.address,
+    });
+    expect(senderBalanceBefore).toBe(parseEther("10000"));
+
+    const recipientBalanceBefore = await publicClient.getBalance({
+      address: recipient,
+    });
+
     await act(async () => {
       renderWithProviders(
         <>
@@ -37,18 +58,21 @@ describe("SendEth", () => {
     });
 
     const recipientInput = screen.getByPlaceholderText("0x...");
-    const amountInput = screen.getByLabelText("Amount (ETH):");
     const sendButton = screen.getByText("Send ETH");
 
     await act(async () => {
       fireEvent.change(recipientInput, {
-        target: { value: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8" },
+        target: { value: recipient },
       });
-      fireEvent.change(amountInput, { target: { value: "0.1" } });
       fireEvent.click(sendButton);
     });
 
-    // await waitFor(() => expect(screen.getByTestId("pending")).toBeDefined());
     await waitFor(() => expect(screen.getByTestId("success")).toBeDefined());
+
+    const recipientBalanceAfter = await publicClient.getBalance({
+      address: recipient,
+    });
+
+    expect(recipientBalanceAfter - recipientBalanceBefore).toBe(amount);
   });
 });
